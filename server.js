46942d3030b8a6fd63fd6fdf2f204e83b055f034
abhay-zel <abhay.pratap@zel.finance>
2024-01-04 10:35:27 +0530

    connect to database

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+})
+
 const app = express();
 
 app.listen(3000, () => {
